4596a23a02ec3d6c29d994e191cf866b64e5fd43
TrongAnh <tronganhnguyen222@gmail.com>
2020-08-04 22:44:01 +0700

    calculator

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@ const Add4Numbers = () => {
   var second_number = document.getElementById('sn').value;
   var third_number = document.getElementById('tn').value;
   var fourth_number = document.getElementById('fon').value;
-  alert(first_number);
-  alert(second_number);
-  alert(third_number);
-  alert(fourth_number);
+  alert("Your first number is:"  + " " + first_number);
+  alert("Your second number is:" + " " + second_number);
+  alert("Your third number is:"  + " " + third_number);
+  alert("Your fourth number is:" + " " + fourth_number);
   var result = (parseInt(first_number) + parseInt(second_number) + parseInt(third_number) + parseInt(fourth_number));
-  alert(result);
+  alert("Your result is:" + " " + result);
   document.getElementById("result").innerHTML = result;
 }
 
@@ -17,12 +17,12 @@ const Substract4Numbers = () => {
   var second_number = document.getElementById('sn').value;
   var third_number = document.getElementById('tn').value;
   var fourth_number = document.getElementById('fon').value;
-  alert(first_number);
-  alert(second_number);
-  alert(third_number);
-  alert(fourth_number);
+  alert("Your first number is:"  + " " + first_number);
+  alert("Your second number is:" + " " + second_number);
+  alert("Your third number is:"  + " " + third_number);
+  alert("Your fourth number is:" + " " + fourth_number);
   var result = (parseInt(first_number) - parseInt(second_number) - parseInt(third_number) - parseInt(fourth_number));
-  alert(result);
+  alert("Your result is:" + " " + result);
   document.getElementById("result").innerHTML = result;
 }
 
@@ -31,12 +31,12 @@ const Multiply4Numbers = () => {
   var second_number = document.getElementById('sn').value;
   var third_number = document.getElementById('tn').value;
   var fourth_number = document.getElementById('fon').value;
-  alert(first_number);
-  alert(second_number);
-  alert(third_number);
-  alert(fourth_number);
+  alert("Your first number is:"  + " " + first_number);
+  alert("Your second number is:" + " " + second_number);
+  alert("Your third number is:"  + " " + third_number);
+  alert("Your fourth number is:" + " " + fourth_number);
   var result = (parseInt(first_number) * parseInt(second_number) * parseInt(third_number) * parseInt(fourth_number));
-  alert(result);
+  alert("Your result is:" + " " + result);
   document.getElementById("result").innerHTML = result;
 }
 
@@ -45,11 +45,11 @@ const Devide4Numbers = () => {
   var second_number = document.getElementById('sn').value;
   var third_number = document.getElementById('tn').value;
   var fourth_number = document.getElementById('fon').value;
-  alert(first_number);
-  alert(second_number);
-  alert(third_number);
-  alert(fourth_number);
+  alert("Your first number is:"  + " " + first_number);
+  alert("Your second number is:" + " " + second_number);
+  alert("Your third number is:"  + " " + third_number);
+  alert("Your fourth number is:" + " " + fourth_number);
   var result = (parseInt(first_number)/parseInt(second_number)/parseInt(third_number)/parseInt(fourth_number));
-  alert(result);
+  alert("Your result is:" + " " + result);
   document.getElementById("result").innerHTML = result;
 }
